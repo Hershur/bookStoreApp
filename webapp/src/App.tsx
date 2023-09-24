7a374b23c51b7d9ea3d2cc113ef2f29828881575
Assurance Femi <assurance.femi@raenest.com>
2023-09-24 21:44:44 +0100

    fix: error handling

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,26 +2,38 @@ import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState<Record<string, never>[]>([]);
+  const [data, setData] = useState<Record<string, never>[]>();
   const [loading, setLoading] = useState(true);
   const [tableName, setTableName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const [formData, setFormData] = useState<Record<string, string>>({});
 
-  const tableHeaders = data.length > 0 && Object.keys(data[0]);
+  const tableHeaders = data && data?.length > 0 && Object.keys(data[0]);
 
   const fetchData = useCallback(async (table: string) => {
+    const url = `http://localhost:5500/fetch/${table || 'user'}`;
+    // const result = await fetch(url);
+    // const response = await result.json();
 
-    try {
-      const url = `http://localhost:5500/fetch/${table || 'user'}`;
-      const result = await fetch(url);
-      const response = await result.json();
+    fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject(response);
+    })
+    .then((response) => {
       setLoading(false);
       setData(response.data);
-      
-    } catch (error) {
-      setLoading(false);
-      console.error(error);
-    }
+    })
+    .catch((response) => {
+      response.json().then((json: unknown) => {
+        setLoading(false);
+        const errorResponse = (json as {message: string, data: unknown});
+        setErrorMessage(errorResponse.message);
+        setData(errorResponse.data as undefined);
+      })
+    })
 
   }, []);
 
@@ -56,14 +68,15 @@ function App() {
 
       <div className="table-wrapper">
           {loading && <strong>Loading data...</strong>}
-          {!loading && data.length === 0 && <strong>No records....</strong>}
+          {!loading && data?.length === 0 && <strong>No records....</strong>}
+          {errorMessage && <strong>{errorMessage}</strong>}
           {
-            !loading && data.length > 0 &&
+            !loading && data && data?.length > 0 &&
             <table className="fl-table">
                 <thead>
                   <tr>
                       {
-                        tableHeaders && tableHeaders.length > 0 && tableHeaders.map(header => (
+                        tableHeaders && tableHeaders?.length > 0 && tableHeaders.map(header => (
                           <th key={header}>{header}</th>
                         ))
                       }
@@ -77,8 +90,8 @@ function App() {
                       return (
                         <tr key={record.id}>
                             {
-                              objRecord.map(x => (
-                                <td>{x}</td>
+                              objRecord.map((x, i)=> (
+                                <td key={i}>{x}</td>
 
                               ))
                             }
